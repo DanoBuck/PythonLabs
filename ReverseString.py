3c05d01e29a904053a0c111a7868f61e973c58a3
DanoBuck <daniielbuckleyty3@gmail.com>
2017-05-01 17:44:41 +0100

    Reversing strings in different approaches

---
+++ ReverseString.py
@@ -0,0 +1,25 @@
+def reverseString(reverseMe):
+    string = ""
+    for i in reversed(reverseMe):
+        string += i
+    return string
+
+
+print("*****Reverse String Iteratively*****")
+print(reverseString("Reverse Me"))
+
+def reverseRecursively(reverseMe):
+    if len(reverseMe) == 0:
+        return ""
+    return reverseMe[len(reverseMe)-1] +  reverseRecursively(reverseMe[:len(reverseMe) - 1])
+
+print("*****Reverse String Recursively*****")
+print(reverseRecursively("Reverse Me"))
+print(reverseRecursively("I want to be reversed recursively"))
+
+def simpleReverse(string):
+    return string[::-1]
+
+print("*****Reverse String Array Negative Indexing*****")
+print(simpleReverse("Reverse Me"))
+print(simpleReverse("I want to be reversed recursively"))
