417602401d4557a254cf52231ddee8526cf3f3c0
DanoBuck <daniielbuckleyty3@gmail.com>
2017-08-07 12:39:08 +0100

    Removing duplicates recursively

--- RemoveDuplicates.py
+++ RemoveDuplicates.py
@@ -7,11 +7,37 @@ def removeDuplicates(listIn):
       nonDuplicated.append(x)
   	  
   print(sorted(nonDuplicated))
-  
 
-inputs = [5,20,25,15,100,200,25,100,75]
-removeDuplicates(inputs)
+
+def remove_duplicates_recursively(list):
+    return remove_duplicates_outer_loop(list, 0, [])
+
+
+def remove_duplicates_outer_loop(list, i, return_list):
+    if len(list) == 1:
+        return list
+
+    if i == len(list):
+        return return_list
+
+    return remove_duplicates_inner_loop(list, i, i + 1, return_list)
+
+
+def remove_duplicates_inner_loop(list, i, j, return_list):
+    if j == len(list):
+        return_list.append(list[i])
+        return remove_duplicates_outer_loop(list, i + 1, return_list)
+
+    if list[i] == list[j]:
+        return remove_duplicates_outer_loop(list, i + 1, return_list)
+
+    return remove_duplicates_inner_loop(list, i, j + 1, return_list)
+
+inputs1 = [4,5,20,25,15,100,200,5,4,11,200,25,100,75]
+recursive_removal = remove_duplicates_recursively(inputs1)
+print(sorted(list(recursive_removal)))
 
 # convert input into a set and then back to a list
-mySet = set(inputs)
+inputs2 = [5,20,25,15,100,200,25,100,75]
+mySet = set(inputs2)
 print(sorted(list(mySet)))
